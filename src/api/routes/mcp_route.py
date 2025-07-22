604381cb47785a6818de179f06455418e0db593f
Tanmaya jain <tanmayajain@deltek.com>
2025-07-22 21:00:07 +0530

    Add email handling tools to MCP server

--- src/api/routes/mcp_route.py
+++ src/api/routes/mcp_route.py
@@ -2,13 +2,26 @@ from fastmcp import FastMCP
 
 mcp = FastMCP(name="emailmcp")
 
-async  def start_mcp_server():
+
+async def start_mcp_server():
     try:
         await mcp.run_async(
             transport="streamable-http",
             host="localhost",
-            port=8001
+            port=8002
         )
 
     except Exception as e:
         raise
+
+
+@mcp.tool(name="send_email", description="Send the email to the recipient given to you.")
+async def send_email(recipient_email_address: str, subject: str, body: str) -> str:
+    return f"email has been sent with subject: {subject} and body {body} to {recipient_email_address}"
+
+
+@mcp.tool(name="search_email",
+          description="Fetch the email on the basis of the search parameters being send.")
+async def search_email(word: str) -> str:
+    # sample email response
+    return "searching for the email"
